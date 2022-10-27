9a115fa0c889687e6a9e81e1c5f6e5dacb29cd0b
kanda999 <kanda@quartile.co>
2022-10-27 07:35:16 +0000

    [ADD] commission sale_commission account_commission

---
+++ sale_commission/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2014-2020 Tecnativa - Pedro M. Baeza
+# Copyright 2020 Tecnativa - Manuel Calero
+{
+    "name": "Sales commissions",
+    "version": "15.0.1.0.0",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_management",
+        "account_commission",
+    ],
+    "website": "https://github.com/OCA/commission",
+    "maintainers": ["pedrobaeza"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/sale_order_view.xml",
+    ],
+    "installable": True,
+}
---
+++ sale_commission/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to define sales agents with their commissions and assign
+them in customers and sales orders.
